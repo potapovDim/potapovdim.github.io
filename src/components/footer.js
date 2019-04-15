7b498c4314ad354c2e9af239322b3af6b44172c6
potapovDim <potapovd3012@gmail.com>
2019-04-15 12:23:01 +0300

    add enzyme adapter

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react'
 
 class Footer extends Component {
   render() {
+    const {projects} = this.props
+    const links = projects.map(({name, url}, index) => <span key={index}>{name} &nbsp {url}</span>)
     return (
-      <div>
-        Footer
-      </div>
+      <div>{links}</div>
     )
   }
 }
--- tests/components/footer.spec.js
+++ tests/components/footer.spec.js
@@ -1,5 +1,16 @@
+import React from 'react'
 import {Footer} from '../../src/components/footer'
+import {shallow} from 'enzyme'
+import {expect} from 'chai'
 
 describe('Footer', () => {
+  const el = shallow(<Footer {...{
+    projects: [{
+      name: 'Test', url: 'https://test.test.com'
+    }]
+  }} />)
 
+  it('props', () => {
+    expect(el.html()).to.include('span')
+  })
 })
